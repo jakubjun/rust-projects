4bd9b74a6ec21aaabc5a6cfb7a5a051e31b12d84
Jun, Jakub <jakubjun21@gmail.com>
2023-01-10 21:44:43 +0100

    Add multiplication table template

---
+++ elementary/07_multiplication_table/src/main.rs
@@ -0,0 +1,22 @@
+// https://adriann.github.io/programming_problems.html
+use std::io;
+
+// Write a program that prints a multiplication table for numbers up to 12.
+
+fn main() -> io::Result<()> {
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{compute_product, compute_sum};
+
+    #[test]
+    fn product() {
+        1
+    }
+
+    #[test]
+    fn sum() {
+        1
+    }
+}
